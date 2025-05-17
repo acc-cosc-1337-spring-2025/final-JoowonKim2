9e32c24932c807db8a77a717c428aeeece72bcd6
JoowonKim2 <jane57975797@gmail.com>
2025-05-17 14:12:48 +0000

    Q1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,15 @@
-//
+#include "die.h"
+#include <ctime>
+#include <cstdlib>
+
+int Die::roll()
+{
+    return rand() % 6 + 1;
+}
+
+//private
+
+void Die::set()
+{
+    std::srand(std::time(0));
+}
--- src/die.h
+++ src/die.h
@@ -1,1 +1,14 @@
-//
+#ifndef DIE_H
+#define DIE_H
+
+class Die
+{
+public:
+    Die() {set();}
+    int roll();
+private:
+    int sides {6};
+    void set();
+};
+
+#endif
