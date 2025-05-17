b2603e85e9bdc090b9e5bfced7379131653a54ce
JoowonKim2 <jane57975797@gmail.com>
2025-05-17 15:39:18 +0000

    Q4

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -44,3 +44,70 @@ TEST_CASE("Test Shooter"){
         REQUIRE(result <= 12);
     }
 }
+
+TEST_CASE("Test ComeOutPhase") {
+
+	Die d1;
+	Die d2;
+	ComeOutPhase phase;
+
+	std::vector<Roll*> rolls;
+	bool get_natural = false, get_craps = false, get_point = false;
+
+	do {
+		Roll roll {d1, d2};
+		roll.roll_dice();
+		rolls.push_back(&roll);
+		
+		if (roll.roll_value() == 7 || roll.roll_value() == 11) {
+			REQUIRE(phase.get_outcome(&roll) == RollOutcome::natural);
+			get_natural = true;
+		}
+		else if (roll.roll_value() == 2 || roll.roll_value() == 3 || roll.roll_value() == 12) {
+			REQUIRE(phase.get_outcome(&roll) == RollOutcome::craps);
+			get_craps = true;
+		}
+		else {
+			REQUIRE(phase.get_outcome(&roll) == RollOutcome::point);
+			get_point = true;
+		}
+
+		if (rolls.size() >= 24)
+			break;
+		
+	} while (!get_natural || !get_craps || !get_point);
+}
+
+TEST_CASE("Test PointPhase") {
+
+	Die d1;
+	Die d2;
+	int point = 5;
+	PointPhase phase(point);
+
+	std::vector<Roll*> rolls;
+	bool get_point = false, get_sevenout = false, get_nopoint = false;
+
+	do {
+		Roll roll {d1, d2};
+		roll.roll_dice();
+		rolls.push_back(&roll);
+
+		if (roll.roll_value() == 7) {
+			REQUIRE(phase.get_outcome(&roll) == RollOutcome::seven_out);
+			get_sevenout = true;
+		}
+		else if (roll.roll_value() == point) {
+			REQUIRE(phase.get_outcome(&roll) == RollOutcome::point);
+			get_point = true;
+		}
+		else {
+			REQUIRE(phase.get_outcome(&roll) == RollOutcome::nopoint);
+			get_nopoint = true;
+		}
+
+		if (rolls.size() >= 24)
+			break;
+
+	} while (!get_point || !get_sevenout || !get_nopoint);
+}
